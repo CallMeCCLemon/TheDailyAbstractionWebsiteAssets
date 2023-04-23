3f63a58ba1f84c59cf739e5225f2e787faf8e0ad
Tonipenyallop <kimtesu521@gmail.com>
2023-04-23 21:53:20 +0900

    add: new content page

---
+++ src/components/blogContent/BlogContent.tsx
@@ -0,0 +1,36 @@
+import { makeStyles, TextField, Typography } from "@material-ui/core";
+import React from "react";
+const useStyles = makeStyles((theme) => ({
+  splashText: {
+    color: theme.palette.primary.contrastText,
+  },
+  "padding-md": {
+    padding: theme.spacing(10),
+  },
+  "padding-sm": {
+    padding: theme.spacing(3),
+  },
+}));
+const BlogContet: React.FC = () => {
+  const classes = useStyles();
+  const contents = ["Title", "Content", "Blurb/Summary", "Tags"];
+  return (
+    <div className={classes["padding-md"]}>
+      {contents.map((content, idx) => (
+        <div className={classes["padding-sm"]} key={`${content}-${idx}`}>
+          <Typography variant={"h3"}>{`${content}:`}</Typography>
+          <TextField
+            fullWidth
+            label={`${content}:`}
+            id={`${content}Id-${idx}`}
+            variant="outlined"
+            multiline={content === "Content"}
+            minRows={content === "Content" ? 10 : 0}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogContet;
