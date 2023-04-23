021ff9e0e37a3814bfa5b1c0b801e956c590912a
Tonipenyallop <kimtesu521@gmail.com>
2023-04-23 18:21:13 +0900

    update: css for sign-in button and nav bar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,36 +1,47 @@
-import {makeStyles} from "@material-ui/core/styles";
-import {AppBar, Grid} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, Grid } from "@material-ui/core";
 import React from "react";
-import {AppRoute} from "../AppRouter";
-import {NavLinkButton} from "./NavLinkButton";
+import { AppRoute } from "../AppRouter";
+import { NavLinkButton } from "./NavLinkButton";
 import SignInButton from "./SignInButton";
 
-const useStyles = makeStyles(theme => ({
-    appbar: {
-        color: theme.palette.primary.main
-    },
-    button: {
-        fontFamily: "Amazon Ember",
-        padding: theme.spacing(1),
-        color: theme.palette.primary.contrastText
-    }
+const useStyles = makeStyles((theme) => ({
+  appbar: {
+    color: theme.palette.primary.main,
+    display: "flex",
+  },
+  button: {
+    fontFamily: "Amazon Ember",
+    padding: theme.spacing(1),
+    color: theme.palette.primary.contrastText,
+  },
+  "grid-container": {
+    display: "flex",
+  },
 }));
 
 const Navbar: React.FC = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <AppBar position={"static"} className={classes.appbar}>
-            <Grid container justifyContent={"center"}>
-                <Grid item xs={12}>
-                    <NavLinkButton  route={AppRoute.HOME} text={"Home"}/>
-                    <NavLinkButton  route={AppRoute.BLOG} text={"Blog"}/>
-                    <NavLinkButton  route={AppRoute.CONTACT} text={"Contact"}/>
-                    <SignInButton />
-                </Grid>
-            </Grid>
-        </AppBar>
-    )
-}
+  return (
+    <AppBar position={"static"} className={classes.appbar}>
+      <Grid container justifyContent={"flex-start"}>
+        <Grid
+          item
+          xs={12}
+          justifyContent={"space-between"}
+          className={classes["grid-container"]}
+        >
+          <div>
+            <NavLinkButton route={AppRoute.HOME} text={"Home"} />
+            <NavLinkButton route={AppRoute.BLOG} text={"Blog"} />
+            <NavLinkButton route={AppRoute.CONTACT} text={"Contact"} />
+          </div>
+          <SignInButton />
+        </Grid>
+      </Grid>
+    </AppBar>
+  );
+};
 
 export default Navbar;
--- src/components/navbar/SignInButton.tsx
+++ src/components/navbar/SignInButton.tsx
@@ -1,27 +1,35 @@
-import {makeStyles} from "@material-ui/core/styles";
-import {Button} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button, Typography } from "@material-ui/core";
 import React from "react";
-import {USER_POOL_APP_CLIENT_ID} from "../../constants";
+import { USER_POOL_APP_CLIENT_ID } from "../../constants";
 
-const useStyles = makeStyles((theme => ({
+const useStyles = makeStyles((theme) => ({
   button: {
     fontFamily: "Amazon Ember",
-    padding: theme.spacing(1),
-    color: theme.palette.primary.contrastText,
-    borderRadius: '16px'
-  }
-})));
+    margin: theme.spacing(1),
+    color: theme.palette.warning.contrastText,
+    borderRadius: "16px",
+    background: theme.palette.warning.main,
+    "&:hover": {
+      color: theme.palette.primary.contrastText,
+    },
+  },
+}));
 
 const SignInButton: React.FC = () => {
   const classes = useStyles();
 
-  const loginPageUrl = `https://auth.thedailyabstraction.com/login?response_type=token&client_id=${USER_POOL_APP_CLIENT_ID}&redirect_uri=https%3A%2F%2Fthedailyabstraction.com%2F`
+  const loginPageUrl = `https://auth.thedailyabstraction.com/login?response_type=token&client_id=${USER_POOL_APP_CLIENT_ID}&redirect_uri=https%3A%2F%2Fthedailyabstraction.com%2F`;
 
   // TODO: This will need to change to Sign Out when a user is logged in.
-  return <Button className={classes.button}
-    onClick={() => window.location.replace(loginPageUrl)}>
-    Log In
-  </Button>
-}
+  return (
+    <Button
+      className={classes.button}
+      onClick={() => window.location.replace(loginPageUrl)}
+    >
+      <Typography variant={"h6"}>Log In</Typography>
+    </Button>
+  );
+};
 
 export default SignInButton;
