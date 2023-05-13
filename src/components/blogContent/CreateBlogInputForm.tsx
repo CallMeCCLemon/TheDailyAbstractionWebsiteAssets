1c85dd553733befe2c707a1cfce5ee6623ef8d6f
Tonipenyallop <kimtesu521@gmail.com>
2023-05-13 19:36:23 +0900

    add: create button

--- src/components/blogContent/CreateBlogInputForm.tsx
+++ src/components/blogContent/CreateBlogInputForm.tsx
@@ -1,10 +1,18 @@
-import { makeStyles } from "@material-ui/core";
+import { Button, makeStyles } from "@material-ui/core";
 
 import React from "react";
 import BlogElement from "./BlogElement";
 const useStyles = makeStyles((theme) => ({
   contentContainer: {
     padding: theme.spacing(10),
+  },
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  postButton: {
+    background: theme.palette.primary.main,
   },
 }));
 const CreateBlogInputForm: React.FC = () => {
@@ -15,6 +23,10 @@ const CreateBlogInputForm: React.FC = () => {
       <BlogElement fieldName={"Content"} multiline={true} minRows={10} />
       <BlogElement fieldName={"Summary"} multiline={false} minRows={0} />
       <BlogElement fieldName={"Tags"} multiline={false} minRows={0} />
+
+      <div className={classes.buttonContainer}>
+        <Button className={classes.postButton}>Create</Button>
+      </div>
     </div>
   );
 };
